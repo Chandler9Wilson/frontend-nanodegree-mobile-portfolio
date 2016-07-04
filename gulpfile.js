f9ae841c0b79450eee31f55933675c9d419cf9be
chandler9wilson <chandler9wilson@gmail.com>
2016-07-04 17:41:26 -0500

    Updated gulpfile to include PSI and ngrok

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ var rename = require('gulp-rename');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
+var ngrok = require('ngrok');
+var psi = require('psi');
+var sequence = require('run-sequence');
+var site = '';
 
 // Lint Task
 gulp.task('lint', function() {
@@ -27,18 +31,83 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
-//Minify CSS, and Images
+// Minify HTML
+gulp.task('html', function() {
+    
+});
 
-//reloads browser when .pipe(browserSync.reload({stream: true})) is run in another task
+// Minify Images
+gulp.task('images', function() {
+
+});
+
+// Concatenate & Minify CSS
+gulp.task('css', function() {
+
+});
+
+// Cleans dist by deleting all files in dist
+gulp.task('clean', function() {
+
+});
+
+// Reloads browser when .pipe(browserSync.reload({stream: true})) is run in another task
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: './src'
     },
-  })
+  });
+});
+
+// Allows a local server to tunnel to a public address
+gulp.task('ngrok-url', function(cb) {  //cb = callback
+    return ngrok.connect(3000, function (err, url) {
+        site = url;
+        console.log('serving your tunnel from: ' + site);
+        cb();
+    });
+});
+
+// A console readout of page speed insights for desktop
+gulp.task('psi-desktop', function (cb) {
+  return psi(site, {
+    nokey: 'true',
+    strategy: 'desktop'
+  }).then(function (data) {
+      console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+      //console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
+  });
+});
+
+// A console readout of page speed insights for mobile
+gulp.task('psi-mobile', function (cb) {
+  return psi(site, {
+    nokey: 'true',
+    strategy: 'mobile'
+  }).then(function (data) {
+      console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+      //console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
+  });
+});
+
+// Sequence to run ngrok, grab ngrok url, and run psi
+gulp.task('psi-seq', function (cb) {
+  return sequence(
+    'ngrok-url',
+    'psi-desktop',
+    'psi-mobile',
+    cb
+  );
+});
+
+// "$gulp psi" runs page speed insights
+gulp.task('psi', ['psi-seq'], function() {
+  console.log('Woohoo! Check out your page speed scores!')
+  process.exit();
 })
 
-//the task run when '$ gulp' is run
+// The task run when '$ gulp' is run
 gulp.task('default', function() {
   'default', ['lint', 'scripts', 'watch']
 });
